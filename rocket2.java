b30abac21dc761dbdeb96edd354a43cd3ad812b2
Gsv123 <127535614+Gsv123@users.noreply.github.com>
2023-12-07 21:24:31 +0530

    Update rocket2.java

--- rocket2.java
+++ rocket2.java
@@ -1,5 +1,9 @@
-int a = 12;
-int b = 17;
-int c = 342;
-int d = a + b + c;
-System. out. println("Addition result = " + d);
+public class addition {
+ int a = 12;
+ int b = 17;
+ int c = 342;
+ int d = a + b + c;
+public static void main(string[]args){  
+  System. out. println("Addition result = " + d);
+ }
+}
